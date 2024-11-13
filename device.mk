2d9e216a310f8b1b650946f832e8cd2e2c296f36
Ahmed Shahin <83367407+GitFASTBOOT@users.noreply.github.com>
2024-11-13 07:48:21 +0200

    gta4l: change variable place
    to avoid this: 
    Run cd android-recovery
    In file included from build/make/core/config.mk:313:
    In file included from build/make/core/envsetup.mk:323:
    In file included from build/make/core/board_config.mk:187:
    device/samsung/gta4l/BoardConfig.mk:92: error: cannot assign to readonly variable: PRODUCT_USE_DYNAMIC_PARTITIONS
    05:41:58 dumpvars failed with: exit status 1

--- device.mk
+++ device.mk
@@ -10,6 +10,8 @@ LOCAL_PATH := device/samsung/gta4l
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
 # VNDK
 PRODUCT_TARGET_VNDK_VERSION := 31
 
